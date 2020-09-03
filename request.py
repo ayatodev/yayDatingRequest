e9dc29c9afc7c995c12d52e368c47a7464a4956e
Ayato <40013127+ayatodev@users.noreply.github.com>
2020-09-03 16:56:42 -0300

    Create request.py

---
+++ request.py
@@ -0,0 +1,174 @@
+#
+# yayDatingRequest
+# 28.02.2020
+# por Ayato
+#
+# github.com/AyatoDev
+#
+
+# -*- coding: utf-8 -*-
+
+import time
+
+time.sleep(0.3)
+print("")
+print("\033[93m" + " - " + "\033[92m" + "yayDatingRequest")
+print("\033[93m" + " - " + "\033[92m" + "28.02.2020")
+time.sleep(0.3)
+
+time.sleep(0.3)
+print("")
+print("\033[93m" + " ♥ " + "\033[92m" + "Você está pronta?")
+time.sleep(0.3)
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 1) " + "\033[92m" + "Olá, meu amor. Você provavelmente não está entendendo nada do que está acontecendo, e também não deve saber o motivo de eu estar fazendo isso. Mas eu estou pensando isso desde o dia 11/01/2020 (meu aniversário), então isso não é de agora. \n\nEu estava pensando muito sobre nós dois no dia do meu aniversário, você foi o maior e melhor presente que eu poderia ter. Você é muito importante pra mim, e creio que você já saiba disso. \n\nMeu bem, eu não quero alongar muito isso, porque senão vai ficar muito chato pra você ter que ficar lendo tanta viadagem. Sei que não é uma coisa 'extraordinária', e muito menos com algum valor monetária, mas eu fiz com muito amor e carinho. \n\nQueria fazer isso de uma forma 'inteligente' e o menos clichê possível. Eu espero que você goste, fiz de coração, eu te amo muito, minha princesa! \n\nE vamos para a primeira questão, você está bem?")
+print("")
+print("\033[93m" + " a) " + "\033[92mSim.\n" + "\033[93m" + " b) " + "\033[92mNão.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "a":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Fico feliz que esteja bem.")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Espero que você fique feliz com o que vou te propor...")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 2) " + "\033[92m" + "Você sabe o motivo de eu estar fazendo isso? Ou você imagina o que vou fazer? Não pense muito agora, responda o que vier na sua cabeça.")
+print("")
+print("\033[93m" + " a) " + "\033[92mSim.\n" + "\033[93m" + " b) " + "\033[92mNão.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "a":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Veremos se sabe mesmo.")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Melhor assim, afinal você não deve saber, até porque é uma surpresa.")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 3) " + "\033[92m" + "Você se lembra de como a gente se conheceu?")
+print("")
+print("\033[93m" + " a) " + "\033[92mSim.\n" + "\033[93m" + " b) " + "\033[92mNão.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "a":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Então você se lembra? Me conte como foi.")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Poxa, achei que você lembrava.")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 4) " + "\033[92m" + "Essa questão vai parecer meio boba, mas me diz a soma de 11 + 17?")
+print("")
+print("\033[93m" + " a) " + "\033[92m31.\n" + "\033[93m" + " b) " + "\033[92m28.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "b":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Ok, você acertou. Memorize esse número.")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "VOCÊ ERROU!")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 5) " + "\033[92m" + "Você se lembra da resposta anterior? Então, agora eu te pergunto, que dia é hoje?")
+print("")
+print("\033[93m" + " a) " + "\033[92m25.\n" + "\033[93m" + " b) " + "\033[92m26.\n" + "\033[93m" + " c) " + "\033[92m27.\n" + "\033[93m" + " d) " + "\033[92m28.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A', 'B', 'C' ou 'D' para responder esta questão. ")
+    if response == "d":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Ok, você acertou.")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "VOCÊ ERROU, PORRA.")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 6) " + "\033[92m" + "Sobre os números da soma anterior. Dia 17, te lembra alguma coisa?")
+print("")
+print("\033[93m" + " a) " + "\033[92mSim.\n" + "\033[93m" + " b) " + "\033[92mNão.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "a":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "O que te lembra esse dia?")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Camila, dia 17 é o seu aniversário...")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 7) " + "\033[92m" + "E o dia 11, o que te lembra?")
+print("")
+print("\033[93m" + " a) " + "\033[92mMe lembra algo.\n" + "\033[93m" + " b) " + "\033[92mNão me lembro de nada.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "a":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Me diz, o que o dia 11 te lembra?")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Puta que pariu, meu aniversário é dia 11, animal!")
+        break
+
+time.sleep(3)
+print("")
+print("\033[93m" + " 8) " + "\033[92m" + "Amor, agora eu vou te explicar o motivo disso. Eu queria transformar o dia 28 em algo especial pra nós, a soma de nossas datas de aniversário dá 28, que é o dia de hoje, o que nos transforma em uma união. \n\nEu não sei como ou onde você está lendo isso agora, mas neste momento que estou escrevendo, estou pensando em algum modo de nos vermos no dia 28, nem que seja por 10 minutos pra eu te mostrar esse 'joguinho' que fiz pra você. \n\nIsso vai parecer meio clichê, o que não era meu objetivo, mas eu queria pensar em uma forma diferente, e não fazer isso igual todo mundo faz. \n\nQuando eu te conheci, nunca imaginei que chegaríamos no ponto que chegamos hoje. A gente não namora, mas desde o dia que eu me apaixonei por você, eu te considero a minha namorada, mesmo não sendo. Eu quero que você entenda que eu sempre vou te amar, te proteger, te dar carinho sempre, sempre e sempre. Você é o meu castelo, vou te proteger a qualquer custo. Eu te amo muito por me amar, minha princesa. \n\nPosso não demonstrar tanto, mas eu quero que você saiba o que eu sinto por você, é gigante. Não tem um dia que eu não pense em você e que eu não sinta saudades, meu amor. Você abriu meus olhos pra muita coisa, e eu sou muito grato por isso. Você despertou algo que eu nunca tinha sentido antes, me fez demonstrar coisas que eu nunca tinha demonstrado antes. No dia 25/02, eu te perguntei qual linguagem de programação você queria aprender, pois eu queria te ensinar. Você me respondeu que queria aprender Python, e estou escrevendo esse joguinho em Python (se você quiser te explico o código depois kkk, aí você aproveita e aprende um pouco <3). Mas agora, eu vou te fazer a última questão... \n\nA soma dos nossos aniversários resulta em 28, hoje é dia 28. Nesta data que é a soma de nossos aniversários; você aceita ser minha namorada? Responda olhando pra mim...")
+print("")
+print("\033[93m" + " a) " + "\033[92mSim.\n" + "\033[93m" + " b) " + "\033[92mNão.\n")
+time.sleep(0.3)
+while True:
+    response = input("\033[93m" + " ♥ " + "\033[92m" + "Digite 'A' ou 'B' para responder esta questão. ")
+    if response == "a":
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Eu te amo, namorada!")
+        break
+    else:
+        time.sleep(1)
+        print("")
+        print("\033[93m" + " ♥ " + "\033[92m" + "Ah, ok.")
+        break
